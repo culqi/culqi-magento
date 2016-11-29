ba10cb12a0bca77edaf6e7d30ab08f0e48017100
Brayan Cruces <brayan2259@gmail.com>
2016-11-29 11:52:20 -0500

    Subida de version v1..0.0
    Plugin con conexión a API v1.0.0

--- app/code/community/Culqi/Pago/controllers/PaymentController.php
+++ app/code/community/Culqi/Pago/controllers/PaymentController.php
@@ -1,5 +1,5 @@
 <?php
-// app/code/local/Envato/Custompaymentmethod/controllers/PaymentController.php
+// PaymentController.php
 class Culqi_Pago_PaymentController extends Mage_Core_Controller_Front_Action
 {
   public function gatewayAction()
@@ -8,7 +8,8 @@ class Culqi_Pago_PaymentController extends Mage_Core_Controller_Front_Action
     {
       $arr_querystring = array(
         'flag' => 1,
-        'orderId' => $this->getRequest()->get("orderId")
+        'orderId' => $this->getRequest()->get("orderId"),
+        'statusOrder' => $this->getRequest()->get("statusOrder")
       );
 
       Mage_Core_Controller_Varien_Action::_redirect('pago/payment/response', array('_secure' => false, '_query'=> $arr_querystring));
@@ -24,7 +25,6 @@ class Culqi_Pago_PaymentController extends Mage_Core_Controller_Front_Action
 
     $checkout = Mage::getSingleton('checkout/session');
     $orderId =  $checkout->getLastRealOrderId();
-
 
     // Obteniendo Data de la sesion
     $order = Mage::getModel('sales/order')->loadByIncrementId($orderId);
@@ -119,7 +119,7 @@ class Culqi_Pago_PaymentController extends Mage_Core_Controller_Front_Action
   {
 
     // SI esta todo correcto
-    if ($this->getRequest()->get("flag") == "1" && $this->getRequest()->get("orderId"))
+    if ($this->getRequest()->get("flag") == "1" && $this->getRequest()->get("orderId") && $this->getRequest()->get("statusOrder") == 'done')
     {
       $orderId = $this->getRequest()->get("orderId");
       $order = Mage::getModel('sales/order')->loadByIncrementId($orderId);
@@ -130,9 +130,54 @@ class Culqi_Pago_PaymentController extends Mage_Core_Controller_Front_Action
       Mage::getSingleton('checkout/session')->unsQuoteId();
       Mage_Core_Controller_Varien_Action::_redirect('checkout/onepage/success', array('_secure'=> false));
     }
+
+    elseif ($this->getRequest()->get("flag") == "1" && $this->getRequest()->get("orderId") && $this->getRequest()->get("statusOrder") == 'fail') {
+
+      $orderId = $this->getRequest()->get("orderId");
+      $order = Mage::getModel('sales/order')->loadByIncrementId($orderId);
+
+      $order->setState(Mage_Sales_Model_Order::STATE_CANCELED, true, 'La orden no fue completada por problemas en el pago.');
+      $order->save();
+
+      // Devolver sesion
+      $session = Mage::getSingleton('checkout/session');
+      if ($session->getLastRealOrderId())
+      {
+          $order = Mage::getModel('sales/order')->loadByIncrementId($session->getLastRealOrderId());
+          if ($order->getId())
+          {
+              //Cancel order
+              if ($order->getState() != Mage_Sales_Model_Order::STATE_CANCELED)
+              {
+                  $order->registerCancellation("Cancelado por el usuario.")->save();
+              }
+              $quote = Mage::getModel('sales/quote')->load($order->getQuoteId());
+              //Return quote
+              if ($quote->getId())
+              {
+                  $quote->setIsActive(1)
+                      ->setReservedOrderId(NULL)
+                      ->save();
+                  $session->replaceQuote($quote);
+              }
+
+              //Unset data
+              $session->unsLastRealOrderId();
+          }
+      }
+
+
+      return $this->getResponse()->setRedirect( Mage::getUrl('checkout/onepage'));
+
+    }
+
     else
     {
+
       Mage_Core_Controller_Varien_Action::_redirect('checkout/onepage/failure', array('_secure'=> false));
     }
+
+
+
   }
 }
