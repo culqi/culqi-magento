1fb514601433097660a012c130bbfa8b94da500d
Juan Ysen Paiba <juanysenpaiba@MacBook-Air-de-Juan.local>
2022-07-26 10:01:44 -0500

    issues QA

--- Controller/Webhook/Event.php
+++ Controller/Webhook/Event.php
@@ -44,13 +44,10 @@ class Event extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
         
         $input = json_decode($inputJSON);
         
-        //var_dump(serialize($input)); exit(1);
-        //$this->logger->debug(serialize($input)); 
-        //var_dump($input->data); exit(1);
         $data = json_decode($input->data);
-        //var_dump($data->metadata->mgt_order_id); exit(1);
+
         $this->logger->debug("Mensaje de webhook recibido");
-        //var_dump($input->type); exit(1);
+
         $this->logger->debug($data);
         if ($input->object == 'event' && $input->type == 'order.status.changed') {
             $mgtOrderId = (int)$data->metadata->mgt_order_id;
