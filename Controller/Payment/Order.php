4d2813389edb6b3e5cfb6b185cb283457221d73e
Juan Ysen Paiba <juanysenpaiba@MacBook-Air-de-Juan.local>
2022-09-03 20:38:19 -0500

    issues QA

--- Controller/Payment/Order.php
+++ Controller/Payment/Order.php
@@ -58,9 +58,6 @@ class Order extends \Magento\Framework\App\Action\Action implements CsrfAwareAct
 
     public function execute()
     {
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
         // Reception of Post parameters
         $orderId = $this->_checkoutSession->getOrderId(); 
         $total = $this->_checkoutSession->getAmount();
